48c2bdfb16414a585ec5fb7f331c598dd582738e
Reness <reness9910@gmail.com>
2024-07-21 15:35:01 +0800

    add

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\renes\\Downloads\\jenkins-php-selenium-test\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
-sleep 1
+
+# Convert Windows path to Docker-compatible format
+DOCKER_PATH='/mnt/c/Users/renes/Downloads/jenkins-php-selenium-test/jenkins-php-selenium-test/src'
+
+# Run Docker container
+docker run -d -p 80:80 --name my-apache-php-app -v "$DOCKER_PATH":/var/www/html php:7.2-apache
+
+# Sleep for a while to ensure the container starts
+sleep 10
+
 set +x
 
+# Print completion message
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
