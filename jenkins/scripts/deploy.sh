10e094aafc05cc65e80a700e98482dfe3d4f0ed1
Reness <reness9910@gmail.com>
2024-07-21 18:08:26 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -x
 # Remove existing container if it exists
 docker rm -f my-apache-php-app || true
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\renes\\Downloads\\jenkins-php-selenium-test\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /mnt/c/Users/renes/Downloads/jenkins-php-selenium-test/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
